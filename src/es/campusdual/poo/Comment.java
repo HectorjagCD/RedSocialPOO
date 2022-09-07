bd91428305d1e3c7877ff40c3fbef65c1f4ada8a
Héctor Acosta Campus Dual <112854864+HectorjagCD@users.noreply.github.com>
2022-09-07 14:57:16 +0200

    Clases listas sin Metodos
    Se terminaron todas las clases, faltan los metodos y el Main

--- src/es/campusdual/poo/Comment.java
+++ src/es/campusdual/poo/Comment.java
@@ -1,5 +1,62 @@
 package es.campusdual.poo;
 
+import java.time.LocalDate;
+
 public class Comment {
+
+	private String commentText;
+	private LocalDate commentDate;
+	private String userCommentOwner;
+
+	public Comment(String commentText, LocalDate commentDate, String userCommentOwner) {
+		super();
+		this.commentText = commentText;
+		this.commentDate = commentDate;
+		this.userCommentOwner = userCommentOwner;
+	}
+
+	// GETTERS SETTERS
+
+	/**
+	 * @return the commentText
+	 */
+	public String getCommentText() {
+		return commentText;
+	}
+
+	/**
+	 * @param commentText the commentText to set
+	 */
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+
+	/**
+	 * @return the commentDate
+	 */
+	public LocalDate getCommentDate() {
+		return commentDate;
+	}
+
+	/**
+	 * @param commentDate the commentDate to set
+	 */
+	public void setCommentDate(LocalDate commentDate) {
+		this.commentDate = commentDate;
+	}
+
+	/**
+	 * @return the userCommentOwner
+	 */
+	public String getUserCommentOwner() {
+		return userCommentOwner;
+	}
+
+	/**
+	 * @param userCommentOwner the userCommentOwner to set
+	 */
+	public void setUserCommentOwner(String userCommentOwner) {
+		this.userCommentOwner = userCommentOwner;
+	}
 
 }
--- src/es/campusdual/poo/TextPost.java
+++ src/es/campusdual/poo/TextPost.java
@@ -5,31 +5,16 @@ import java.util.List;
 
 public class TextPost extends Post {
 	
-	private String title;
+
 	private String content;
 	
 	public TextPost(LocalDate postDate, List<Comment> commentList, String title, String content) {
 		super(postDate, commentList);
-		this.title = title;
 		this.content = content;
 	}
 
 	// GETTERS SETTERS
 	
-
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * @param title the title to set
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
 
 	/**
 	 * @return the content
